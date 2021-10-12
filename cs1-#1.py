45736c0ea54f60d993379a3e255dbc463b577081
Jordan Hart <p22jhart@fairfieldprep.org>
2021-10-12 07:20:19 -0700

    hw thurs

---
+++ cs1-#1.py
@@ -0,0 +1,18 @@
+
+
+
+x1=input("input x cord")
+x1=float(x1)
+
+y1=input("input y cord")
+y1=float(y1)
+
+x2=input("input x cord")
+x2=float(x2)
+
+y2=input("input x cord")
+y2=float(y2)
+
+a=input 
+
+print(a)
